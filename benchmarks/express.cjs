990af52358655a8136c36601d804c9ac6c72d776
Sebastian Beltran <bjohansebas@gmail.com>
2024-09-17 20:49:18 -0500

    add async to express

--- benchmarks/express.cjs
+++ benchmarks/express.cjs
@@ -7,7 +7,7 @@ const app = express()
 app.disable('etag')
 app.disable('x-powered-by')
 
-app.get('/', function (req, res) {
+app.get('/', async function (req, res) {
   res.json({ hello: 'world' })
 })
 
